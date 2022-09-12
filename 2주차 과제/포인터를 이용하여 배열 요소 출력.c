c6e56d5f6be149ee5e190b64b171dc1431f3b0dd
윤재형 <102245864+wogud1220@users.noreply.github.com>
2022-09-12 23:36:47 +0900

    Update 포인터를 이용하여 배열 요소 출력.c

--- 2주차 과제/포인터를 이용하여 배열 요소 출력.c
+++ 2주차 과제/포인터를 이용하여 배열 요소 출력.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() {
 	int i = 0;
-	double arrd[4] = { 12.4, 23.1, 15.7, 31.2 };
+	double arrd[4] = { 12.4, 23.1, 15.7, 31.2 };	//arrd의 배열에 4개의 실수 저장
 	double* pb = arrd;
 	for (i = 0; i < 4; i++) {
 		printf("%lf\n", pb[i]);
