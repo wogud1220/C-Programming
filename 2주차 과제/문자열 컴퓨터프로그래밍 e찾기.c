568c470912681c924e2e35be04c0538474109fa6
윤재형 <102245864+wogud1220@users.noreply.github.com>
2022-09-12 23:00:00 +0900

    Add files via upload
    문자열 컴퓨터프로그래밍 e찾기

---
+++ 2주차 과제/문자열 컴퓨터프로그래밍 e찾기.c
@@ -0,0 +1,24 @@
+/*#include <stdio.h>
+int main() {
+	char str1[20] = "computerprogramming";
+	int i = 0;
+	for (i = 0; i < 20; i++) {
+		if (str1[i] == 'e') {
+			printf("배열로 찾은 e는 %d번째에 있습니다.", i+1);
+		}
+		
+	}
+
+}*/
+
+#include <stdio.h>
+int main() {
+
+	char* p = "computerprogramming";
+	int i = 0;
+	for (i = 0; i < 20; i++) {
+		if (p[i] == 'e') {
+			printf("포인터로 찾은 e는 %d번째에 있습니다.\n", i+1);
+		}
+	}
+}
