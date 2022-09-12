f872011335e50d7ad31723418451a0a1ade8c7f4
윤재형 <102245864+wogud1220@users.noreply.github.com>
2022-09-12 22:14:48 +0900

    Add files via upload
    포인터를 이용하여 배열 요소 출력.c

---
+++ 2주차 과제/포인터를 이용하여 배열 요소 출력.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+	int i = 0;
+	double arrd[4] = { 12.4, 23.1, 15.7, 31.2 };
+	double* pb = arrd;
+	for (i = 0; i < 4; i++) {
+		printf("%lf\n", pb[i]);
+	}
+	return 0;
+ 
+}
