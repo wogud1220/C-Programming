54dce5c67b44e77ca30eb196a71c8be52062069e
윤재형 <yoonwogud@gmail.com>
2022-09-15 01:16:12 +0900

    finish

---
+++ 개인연습/getchar,putchar 사용.c
@@ -0,0 +1,18 @@
+/*#include <stdio.h>
+void main()
+{
+	char c;
+	printf("문자를 입력하세요\n");
+	c = getchar(32);
+	printf("%c", c);
+	return 0;
+}*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+void main()
+{
+	putchar(65);
+	printf("\n");
+	putchar('a');
+}
