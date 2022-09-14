de92bf6905d5599639366af1b97c8ec2b123d6a8
윤재형 <yoonwogud@gmail.com>
2022-09-15 01:28:38 +0900

    finish

---
+++ 개인연습/화씨,섭씨.c
@@ -0,0 +1,36 @@
+//for 문으로 섭씨, 화씨
+
+/*#include<stdio.h>
+void main()
+{
+	int i, j;
+	i = 0, j = 0;
+
+	printf("   섭씨            화씨\n--------------------------\n");
+
+	for (i = 10; i <= 100; i = i + 10) {
+		j = 9. / 5 * i + 32;
+		printf("%d", i);
+		printf("                        %d\n", j);
+	}
+}*/
+
+
+//while문으로 섭씨, 화씨
+
+/*#include<stdio.h>
+void main()
+{
+	int i, j;
+	i = 0, j = 0;
+
+	printf("   섭씨             화씨\n--------------------------\n");
+
+	while (i <= 90)
+	{
+		i = i + 10;
+		j= 9. / 5 * i + 32;
+		printf("%d", i);
+		printf("                  %d\n",j);
+	}
+}*/
