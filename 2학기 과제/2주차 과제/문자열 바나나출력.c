28450daaca31babf4561b701176652da9ef8da8d
윤재형 <yoonwogud@gmail.com>
2022-09-15 09:42:20 +0900

    f

--- 2학기 과제/2주차 과제/문자열 바나나출력.c
+++ 2학기 과제/2주차 과제/문자열 바나나출력.c
@@ -3,8 +3,8 @@ int main() {
 
 	char* p = "banana";	//포인터 문자열 바나나 선언
 	int i;
-	for (i = 0; i < 6; i++) {	//6번 반복
-		printf("%s\n", (p+i));	//문자열 출력, 포인터 p의 시작 지점을 1씩 뒤로 밀면서
+	for (i = 0; i < sizeof(p)/sizeof(char); i++) {	//6번 반복
+		printf("%s\n", (p + i));	//문자열 출력, 포인터 p의 시작 지점을 1씩 뒤로 밀면서
 	}
 	return 0;	//함수 종료
 }
