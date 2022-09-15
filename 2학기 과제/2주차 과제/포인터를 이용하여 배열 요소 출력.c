c7e38effc2a77dddd3331593701e58605b5f1bb4
윤재형 <yoonwogud@gmail.com>
2022-09-15 09:32:04 +0900

    seasfd

--- 2학기 과제/2주차 과제/포인터를 이용하여 배열 요소 출력.c
+++ 2학기 과제/2주차 과제/포인터를 이용하여 배열 요소 출력.c
@@ -4,7 +4,7 @@ int main() {
 	double arrd[4] = { 12.4, 23.1, 15.7, 31.2 };	//arrd의 배열에 4개의 실수 저장
 	double* pb = arrd;
 	for (i = 0; i < 4; i++) {
-		printf("%lf\n", pb[i]);
+		printf("%lf\n", *(pb+i));
 	}
 	return 0;
  
