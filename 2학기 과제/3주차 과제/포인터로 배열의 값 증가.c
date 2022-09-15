f24751fb01d00b23a3e54036bf8f1a60a010accb
윤재형 <yoonwogud@gmail.com>
2022-09-15 11:14:58 +0900

    f

--- 2학기 과제/3주차 과제/포인터로 배열의 값 증가.c
+++ 2학기 과제/3주차 과제/포인터로 배열의 값 증가.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
-int main(){
-
-int arr[5]
-
-
-
-
-
-
-
-
-
-
+int main() {
+	int i = 0;
+	int arr[5] = { 1,2,3,4,5 };
+	int* ptr = arr;
+	ptr = ptr + 2;
+	while (i < 5) {
+		printf("%d\n", (*ptr +i ));
+		i++;
+	}
 
 
 
