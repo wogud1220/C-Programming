187160e0bf293c8d6b1c782b1eb10b6db64e83a5
wogud1220 <yoonwogud@gmail.com>
2022-09-18 03:01:46 +0900

    finish

--- 개인연습/화씨,섭씨.c
+++ 개인연습/화씨,섭씨.c
@@ -34,3 +34,23 @@ void main()
 		printf("                  %d\n",j);
 	}
 }*/
+
+
+//do while문으로 섭씨, 화씨
+
+/*#include<stdio.h>
+void main()
+{
+	int i, j;
+	i = 0, j = 0;
+
+	printf(" 섭씨             화씨\n--------------------------\n");
+
+	do {
+		i = i + 10;
+		j = 9. / 5 * i + 32;
+		printf("%d", i);
+		printf("                   %d\n", j);
+
+	} while (i <= 90);
+}*/
