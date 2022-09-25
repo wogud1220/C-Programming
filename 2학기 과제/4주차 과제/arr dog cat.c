6158adc48542432936479e8a8c284833251652b5
wogud1220 <yoonwogud@gmail.com>
2022-09-25 19:05:18 +0900

    asdf

--- 2학기 과제/4주차 과제/arr dog cat.c
+++ 2학기 과제/4주차 과제/arr dog cat.c
@@ -2,27 +2,28 @@
 #include <stdio.h>
 int main() {
 	//1.
-	/*char animal2[4][10] = {"dog","cat","horse","elephant"};
-	
-	
+	char animal2[4][10] = { "dog","cat","horse","elephant" };
+
+
 	int i;
 	int cnt;
 	cnt = sizeof(animal2) / 10; //sizeof(animal[0]) 1칸은 10의 크기를 가지고 있음
 	for (i = 0; i < cnt; i++) {
 		printf("%s\n", animal2[i]);
-	}*/
-	//2.
-		char animal[4][10] = {
-		{'c','a','t'},
-		{'d','o','g'},
-		{'h','o','r','s','e'},
-		{'e','l','p','h','a','n','t'}
-		};
-
-		int i;
-		int cnt;
-		cnt = sizeof(animal) / 10; //sizeof(animal[0]) 1칸은 10의 크기를 가지고 있음
-		for (i = 0; i < cnt; i++) {
-			printf("%s\n", animal[i]);
-		}
 	}
+}
+//2.
+	/*char animal[4][10] = {
+	{'c','a','t'},
+	{'d','o','g'},
+	{'h','o','r','s','e'},
+	{'e','l','p','h','a','n','t'}
+	};
+
+	int i;
+	int cnt;
+	cnt = sizeof(animal) / 10; //sizeof(animal[0]) 1칸은 10의 크기를 가지고 있음
+	for (i = 0; i < cnt; i++) {
+		printf("%s\n", animal[i]);
+	}
+}*/
