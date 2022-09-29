791a837824cea96d140d8e1f392541b9119dafb3
wogud1220 <yoonwogud@gmail.com>
2022-09-29 09:56:34 +0900

    asdf

--- 2학기 과제/4주차 과제/ptr 모음 찾기.c
+++ 2학기 과제/4주차 과제/ptr 모음 찾기.c
@@ -1,34 +1,35 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-void mo(char *ptr1, char *ptr2, int* num1, int* num2) {
+void mo(char* ptr1, char* ptr2, int* num1, int* num2) {
 	int cnt1 = 0;
 	int cnt2 = 0;
 	int i = 0;
 
-		for (i = 0; i < sizeof(ptr1) / sizeof(char); i++) {
-			if (*(ptr1+i) == 'a')
-				cnt1++;
-			else if (*(ptr1 + i) == 'e')
-				cnt1++;
-			else if (*(ptr1 + i) == 'i')
-				cnt1++;
-			else if (*(ptr1 + i)== 'o')
-				cnt1++;
-			else if (*(ptr1 + i) == 'u')
-				cnt1++;
-		}
-		for (i = 0; i < sizeof(ptr2) / sizeof(char); i++) {
-			if (*(ptr2+i) == 'a')
-				cnt2++;
-			else if (*(ptr2 + i) == 'e')
-				cnt2++;
-			else if (*(ptr2 + i) == 'i')
-				cnt2++;
-			else if (*(ptr2 + i) == 'o')
-				cnt2++;
-			else if (*(ptr2 + i) == 'u')
-				cnt2++;
-		}
+	for (i = 0; i < 50; i++) {
+		if (*(ptr1 + i) == 'a')
+			cnt1++;
+		else if (*(ptr1 + i) == 'e')
+			cnt1++;
+		else if (*(ptr1 + i) == 'i')
+			cnt1++;
+		else if (*(ptr1 + i) == 'o')
+			cnt1++;
+		else if (*(ptr1 + i) == 'u')
+			cnt1++;
+	}
+	
+	for (i = 0; i < 50; i++) {
+		if (*(ptr2 + i) == 'a')
+			cnt2++;
+		else if (*(ptr2 + i) == 'e')
+			cnt2++;
+		else if (*(ptr2 + i) == 'i')
+			cnt2++;
+		else if (*(ptr2 + i) == 'o')
+			cnt2++;
+		else if (*(ptr2 + i) == 'u')
+			cnt2++;
+	}
 	*num1 = cnt1;
 	*num2 = cnt2;
 	return 0;
@@ -40,8 +41,8 @@ void  main() {
 	char* ptr1 = word1;
 	char* ptr2 = word2;
 	printf("단어를 2개 입력하세요\n");
-	scanf("%s",word1);
-	scanf("%s",word2);
+	scanf("%s", word1);
+	scanf("%s", word2);
 	mo(ptr1, ptr2, &num1, &num2);
 	printf("word1의 모음의 수는 %d개입니다.\n", num1);
 	printf("word2의 모음의 수는 %d개입니다.", num2);
