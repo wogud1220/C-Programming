c49696546e90a527ef1a1fc1f8c089cc26ac48db
wogud1220 <yoonwogud@gmail.com>
2022-11-14 21:45:20 +0900

    asdf

--- 2학기 과제/11주차 과제/문제6_역순출력.c
+++ 2학기 과제/11주차 과제/문제6_역순출력.c
@@ -10,20 +10,21 @@ int main(void) {
 	char buf[100];
 	char list[10][100];
 	int i = 0;
-
-
+	int line = 0;
 	fp = fopen("text.txt", "w+");
 
 	if (fp == NULL) {
 		puts("파일오픈 실패!");
 		return -1;
 	}
-
-	for (i = 0; i < 3; i++) {
-		printf("%d번째 문장 입력 :", i+1);
+	printf("몇 줄을 입력하실건가요\n");
+	scanf("%d", &line);
+	getchar();
+	for (i = 0; i < line; i++) {
+		printf("%d번째 문장 입력 :", i + 1);
 		gets(list[i]);
 		fputs(list[i], fp);
-		fputs("\n",fp);
+		fputs("\n", fp);
 	}
 
 	i = 0;
@@ -31,7 +32,6 @@ int main(void) {
 	fseek(fp, 0, SEEK_SET);
 
 	while (fgets(list[i], sizeof(list[i]), fp) != NULL) {
-
 		printf("%d: %s", i + 1, list[i]);
 
 		i++;
