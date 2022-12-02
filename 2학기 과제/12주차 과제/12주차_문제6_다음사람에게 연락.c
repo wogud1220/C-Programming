e5023b3f6f7112f6d18b171d9c6f49689e11fa5b
wogud1220 <yoonwogud@gmail.com>
2022-12-02 11:21:38 +0900

    asdf

--- 2학기 과제/12주차 과제/12주차_문제6_다음사람에게 연락.c
+++ 2학기 과제/12주차 과제/12주차_문제6_다음사람에게 연락.c
@@ -39,116 +39,66 @@ int main() {
         cnt++;
     }
 
-    printf("%d개의 연락처가 저장됨.\n",cnt);
-    printf("%s이 연락을 받았습니다.모든 구성원들에게 연락을 했습니다.\n", temp->name);
-    printf("아픈 사람을 입력하세요 :");
-    scanf("%s", name);
+
+    printf("%d개의 연락처가 저장됨.\n", cnt);
+    printf("%s가 연락을 받았습니다.모든 구성원들에게 연락을 했습니다.\n", temp->name);
+
     temp = head;
-    while (temp != NULL) {
-        if (!strcmp(name, temp->name)) {
-            printf("%s는 %s에게 연락해야합니다. %s의 전화번호는 %s입니다.\n",
-                name, temp->next->name, temp->next->name, temp->next->phonenum);
-            break;
-        }
-        else
-            temp = temp->next;
-    }
-    
-    temp = head;
-    printf("%s는 %s에게 연락을 해야 합니다. %s의 전화번호는 %s입니다.\n", 
-        head->name, head->next->name, head->next->name, head->next->phonenum);
-    int i = 0;
-
-
-    while(temp!=NULL) {
-        temp = temp->next;
-        printf("%s는 %s에게 연락을 해야 합니다. %s의 전화번호는 %s입니다.\n", 
-            temp->name, temp->next->name, temp->next->name, temp->next->phonenum);
-    }
-
-
-
-    return 0;
-
-
-    
-
-
-}
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#pragma warning(disable: 4996)
-typedef struct data {
-    char name[20];
-    char number[20];
-    struct data* next;
-}data;
-//새로운 노드 생성 해서 데이터 입력받고 노드 반환
-data* insert()
-{
-    data* temp = (data*)malloc(sizeof(data));
-    char name[20];
-    char number[20];
-    printf("이름 : ");
-    scanf("%s", temp->name);
-    printf("전화번호 : ");
-    scanf("%s", temp->number);
-    temp->next = NULL;
-    return temp;
-}
-//연결리스트중 마지막 요소찾아서 반환
-data* find_finish(data* node)
-{
     while (1)
     {
-        if (node->next == NULL)
-            return node;
-        node = node->next;
+        if (temp->next != NULL) {
+            printf("%s는 %s에게 연락해야합니다. %s의 전화번호는 %s입니다.\n",
+                temp->name, temp->next->name, temp->next->name, temp->next->phonenum);
+            temp = temp->next;
+
+        }
+
+        else
+            break;
     }
-}
-int main()
-{
-    data* head;
-    printf("<<연락처 입력>>\n");
-    head = insert();
-    char s[10];
-    for (int i = 1; ; i++)
-    {
-        if (i >= 5)
-        {
-            char menu;
-            printf("5명 이상 입력했습니다. 종료하시겠습니까?\n예 : q\n아니요 : n\n");
-            printf("입력 : ");
-            getchar();
-            scanf("%c", &menu);
-            if (menu == 'q')
+
+    temp = head;
+    /*printf("%s는 %s에게 연락을 해야 합니다. %s의 전화번호는 %s입니다.\n",
+        head->name, head->next->name, head->next->name, head->next->phonenum);*/        //처음 사람 출력
+    int i = 0;
+
+    printf("아픈 사람을 입력하세요 :");
+    scanf("%s", name);
+    printf("\n%s님이 탈퇴했습니다.\n", name);
+    BOOK* sick = head;
+
+    
+
+    while (1) {
+        if (!strcmp(name, temp->name)) {
+            printf("앞으로는 %s는 %s에게 연락을 해야 합니다. %s의 전화번호는 %s입니다.\n",
+                sick->name, temp->next->name, temp->next->name, temp->next->phonenum);
+            break;
+        }
+        /*else if (temp->next == NULL && strcmp(name, temp->name))
+            printf("마지막사람입니다.");*/
+        else {
+            sick = temp;
+            temp = temp->next;
+            if (temp->next == NULL && (!strcmp(name, temp->name))) {
+                printf("%s는 마지막사람입니다.\n", name);
+                printf("%s는 연락할 사람이 없습니다.", sick->name);
                 break;
+            }
         }
-        //마지막 노드 찾아서 마지막노드 뒤에 새로운 노드 연결시키기
-        data* temp = find_finish(head);
-        temp->next = insert();
     }
-    data* cp1 = head;
-    data* cp2 = head;
-    data* cp3 = head;
-    printf("\n<<연락처 출력>>\n");
-    while (cp1->next != NULL)
-    {
-        printf("%s는 %s에게 연락을 해야 합니다. %s의 전화번호는 %s입니다.\n", cp1->name, cp1->next->name, cp1->next->name, cp1->next->number);
-        cp1 = cp1->next;
-    }
-    if (cp1->next == NULL)
-        printf("%s이 연락을 받았습니다. 모든 구성원들에게 연락을 했습니다.\n", cp1->name);
-    printf("구성원 이름을 입력하세요 : ");
-    scanf("%s", s);
-    while (cp3->next != NULL) {
-        if (strcmp(cp3->next->name, s) == 0) {
-            printf("%s는%s에게 연락해야합니다. %s의 전화번호는 %s입니다.",cp3->name,cp3->next->next->name, cp3->next->next->name, cp3->next->next->number);
+
+    temp = head;
+    printf("\n연락처 출력        빠진 사람 제외하고\n");
+    while (temp != NULL) {
+        if (!strcmp(name, temp->name)) {
+            temp = temp->next;
+            continue;
         }
-        cp3 = cp3->next;
+        printf("\n%s : %s\n", temp->name, temp->phonenum);
+        temp = temp->next;
     }
 
     return 0;
 }
+
