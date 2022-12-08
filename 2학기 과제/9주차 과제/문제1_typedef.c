9a0e7d50e24c87423f9b394998c657235dd9f6ab
wogud1220 <yoonwogud@gmail.com>
2022-12-09 04:49:57 +0900

    asdf

--- 2학기 과제/9주차 과제/문제1_typedef.c
+++ 2학기 과제/9주차 과제/문제1_typedef.c
@@ -7,11 +7,14 @@ typedef struct point {
 
     int ypos;
 
-}POINT;
+}POINT;         //POINT 구조체의 멤버 xpos, ypos
+
+
+
 
 
 
 typedef struct score {
 	int number;
 	int exam;
-}SCORE;
+}SCORE;         //SCORE 구조체의 멤버 number, exam
