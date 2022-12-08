0277a964aeb6c2f6cc197fb6dbfdd8424249362e
wogud1220 <yoonwogud@gmail.com>
2022-12-09 04:50:02 +0900

    asdf

--- 2학기 과제/9주차 과제/문제3_strcut swap.c
+++ 2학기 과제/9주차 과제/문제3_strcut swap.c
@@ -6,8 +6,8 @@ typedef struct point {
 	int ypos;
 }POINT;
 
-void swap(POINT* pos1, POINT* pos2) {
-	POINT temp;
+void swap(POINT* pos1, POINT* pos2) {		//받을 때 포인터로 받기
+	POINT temp;		//POINT의 새 변수 temp 생성해서 숫자 자리 바꾸기
 	temp = *pos1;
 	*pos1 = *pos2;
 	*pos2 = temp;
@@ -20,7 +20,7 @@ int main() {
 	POINT pos2 = { 5,7 };
 
 	
-	swap(&pos1, &pos2);
+	swap(&pos1, &pos2);			//swap할때 주소값 넘겨주기
 	printf("바뀐 pos1의 값%d %d\n", pos1.xpos, pos1.ypos);
 	printf("바뀐 pos2의 값%d %d\n", pos2.xpos, pos2.ypos);
 
--- 2학기 과제/9주차 과제/문제4_x,y좌표 사각형 만들기.c
+++ 2학기 과제/9주차 과제/문제4_x,y좌표 사각형 만들기.c
@@ -9,9 +9,9 @@ typedef struct point {
 } POINT;
 
 
-void swap(POINT* pos1, POINT* pos2) {
+void swap(POINT* pos1, POINT* pos2) {           //포인터로 받기
 
-     printf("넓이:%d", (pos2->xpos - pos1->xpos) * (pos2->ypos - pos1->ypos));
+     printf("넓이:%d", (pos2->xpos - pos1->xpos) * (pos2->ypos - pos1->ypos));      //포인터로 받으면 ->로 위치 표시하기
 }
 
 int main() {
@@ -24,5 +24,5 @@ int main() {
     printf("우상단:%d %d\n", pos2.xpos, pos2.ypos);
     printf("좌상단:%d %d\n", pos1.xpos, pos2.ypos);
     printf("우하단:%d %d\n", pos2.xpos, pos1.ypos);
-    swap(&pos1, &pos2);
+    swap(&pos1, &pos2);         // 다른 함수로 보낼때 &붙이기
 }
