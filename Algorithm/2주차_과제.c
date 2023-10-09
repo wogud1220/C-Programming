eed63984df2701621d552d9c36133946879ef291
wogud1220 <yoonwogud@gmail.com>
2023-10-09 17:40:58 +0900

    ㅁㄴㅇㄹ

--- Algorithm/2주차_과제.c
+++ Algorithm/2주차_과제.c
@@ -1,5 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS
-#define MAX(a,b) (((a)>(b))?(a):(b))
+#define MAX(a, b) (((a) > (b)) ? (a) : (b))
 #define MAX_SIZE 1000001
 #define TRUE 1
 #define FALSE 0
@@ -9,135 +9,145 @@
 #include <math.h>
 #include <memory.h>
 #include <ctype.h>
-//#include <windows.h>
-//#pragma warning(disable:4996)
-void digitsum(int n) {
+// #include <windows.h>
+// #pragma warning(disable:4996)
+void digitsum(int n)
+{
 	int d = 0;
-	for (int i = n; i >0; i=i / 10) {
-		 d= d + (i % 10);
-		 printf("%d + ", i%10);
+	for (int i = n; i > 0; i = i / 10)
+	{
+		d = d + (i % 10);
+		printf("%d + ", i % 10);
 	}
 	printf("= %d\n\n", d);
 	return;
 }
 
-
-
-void hanoi(int m, char a, char b, char c) {
+void hanoi(int m, char a, char b, char c)
+{
 	if (m == 1)
-		printf("<1 원판>을 %c 에서 %c로 옮겼습니다.\n",a,c);
-	else {
+		printf("<1 원판>을 %c 에서 %c로 옮겼습니다.\n", a, c);
+	else
+	{
 		hanoi(m - 1, a, c, b);
-		printf("<%d> 원판을 <%c>에서 <%c>로 옮긴다.\n",m, a, c);
-		hanoi(m - 1, b,a,c);
+		printf("<%d> 원판을 <%c>에서 <%c>로 옮긴다.\n", m, a, c);
+		hanoi(m - 1, b, a, c);
 	}
 }
 
-int hanoiCount(int m) {
+int hanoiCount(int m)
+{
 	int num = 1;
-	while (--m) {
+	while (--m)
+	{
 		num = num * 2 + 1;
 	}
 	return num;
 }
 
-	int arr[10] = { 1,2,3,4,5,6,7,8,9,10};
-	int arr2[20]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
-	int arr3[30]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,
-	21,22,23,24,25,26,27,28,29,30};
+int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+int arr2[20] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+int arr3[30] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+				21, 22, 23, 24, 25, 26, 27, 28, 29, 30};
 
-
-int size1=sizeof(arr)/4;
-int size2=sizeof(arr2)/4;
-int size3=sizeof(arr3)/4;
-	int cnt = 1;	//기본 1번 탐색ㄴ
-int binarySearch(int find,int first,int last) {	//배열과 찾을 값 받음
+int size1 = sizeof(arr) / 4;
+int size2 = sizeof(arr2) / 4;
+int size3 = sizeof(arr3) / 4;
+int cnt = 1; // 기본 1번 탐색ㄴ
+int binarySearch(int find, int first, int last)
+{ // 배열과 찾을 값 받음
 	int mid = (first + last) / 2;
 
 	//==========실패할 경우
-	//log2 N 이므로 10일때 3, 20일때 5, 30일때 5 값은 반올림 연산
-	if ((int)ceil(log2(size2))==cnt && (arr[mid] != find) //최대 이동회수에 도달하고 중간값에도 아니라면2
-	|| (arr[last] < find) || arr[first]>find) {		//마지막보다 크거나 처음거보다 작거나
-			printf("%d %d %d %d %d",last, arr[last],find,(arr[last] < find),arr[last-2]);	//반올림연산
+	// log2 N 이므로 10일때 3, 20일때 5, 30일때 5 값은 반올림 연산
+	if ((int)ceil(log2(size2)) == cnt && (arr[mid] != find) // 최대 이동회수에 도달하고 중간값에도 아니라면2
+		|| (arr[last] < find) || arr[first] > find)
+	{																						// 마지막보다 크거나 처음거보다 작거나
+		printf("%d %d %d %d %d", last, arr[last], find, (arr[last] < find), arr[last - 2]); // 반올림연산
 		printf("\n찾지 못했습니다.\n");
 		return 0;
 	}
 	//=========성공할 경우
-	if (arr[first] == find) {
+	if (arr[first] == find)
+	{
 		printf("현재 검색 범위: [%d] ~ [%d]\n찾은 위치:[%d]\n중간 위치:[%d]\n\
-비교하는 값:%d\n", first, last, first, mid, find);
+비교하는 값:%d\n",
+			   first, last, first, mid, find);
 		printf("[%d]index에서 찾음\n", first);
 	}
-	else if (arr[last] == find) {
+	else if (arr[last] == find)
+	{
 		printf("현재 검색 범위: [%d] ~ [%d]\n찾은 위치:[%d]\n중간 위치:[%d]\n\
-비교하는 값(찾은 값):%d\n\n", first, last-1, last, mid, find);
+비교하는 값(찾은 값):%d\n\n",
+			   first, last - 1, last, mid, find);
 		printf("[%d]index에서 찾음\n", last);
 	}
-	else if (arr[mid] == find) {	//mid에서 값을 찾으면
+	else if (arr[mid] == find)
+	{ // mid에서 값을 찾으면
 		printf("현재 검색 범위: [%d] ~ [%d]\n찾은 위치:[%d]\n중간 위치:[%d]\n\
-비교하는 값(찾은 값):%d\n\n", first, last, mid, mid, find);
+비교하는 값(찾은 값):%d\n\n",
+			   first, last, mid, mid, find);
 		printf("[%d]index에서 찾음\n", mid);
 	}
-	else if (arr[mid] > find) {	//찾을 값이 작으면 mid 왼쪽 이동
+	else if (arr[mid] > find)
+	{ // 찾을 값이 작으면 mid 왼쪽 이동
 		printf("%d번 탐색\n현재 검색 범위: [%d] ~ [%d]\n중간 위치의 index:[%d]\n\
-		중간 위치의 값:%d\n", cnt,first, last, mid, arr[mid]);
+		중간 위치의 값:%d\n",
+			   cnt, first, last, mid, arr[mid]);
 		cnt++;
 		binarySearch(find, first, last / 2);
 	}
-	else if (arr[mid] < find) {
+	else if (arr[mid] < find)
+	{
 		printf("%d번 탐색\n현재 검색 범위: [%d] ~ [%d]\n중간 위치:[%d]\n\
-		중간 위치의 값:%d\n", cnt,first, last, mid, arr[mid]);
+		중간 위치의 값:%d\n",
+			   cnt, first, last, mid, arr[mid]);
 		cnt++;
 		binarySearch(find, mid, last);
-		//찾을 값이 크면 mid 오른쪽 이동
+		// 찾을 값이 크면 mid 오른쪽 이동
 	}
 	return cnt;
-	
 }
 
-int main() {
+int main()
+{
 	int num = 0;
 	int n = 0;
-	int m = 3;	//하노이 개수
+	int m = 3; // 하노이 개수
 	char a = 'A';
 	char b = 'B';
 	char c = 'C';
 
 	int first = 0;
-	int last = (sizeof(arr)/4)-1;
-	printf("arr last= %d",last);
-	int cnt=0;
+	int last = (sizeof(arr) / 4) - 1;
+	int cnt = 0;
 	int dec = 0;
 
-		printf("1: digitsum()\n2: binarySearch()\n3: hanoi()\n");
-		scanf("%d", &num);
-		switch (num)
-		{
-		case 1:
-			printf("정수를 입력하세요.\n");
-			scanf("%d", &n);
-			digitsum(n);
-			break;
-		case 2:
-			printf("찾을 값을 입력하세요.\n");
-			scanf("%d", &dec);
-			cnt=binarySearch(dec,first,last);
-			if(cnt>=1)
-				printf("%d번째 탐색에서 찾음\n", cnt);
-			break;
-		case 3:
-			hanoi(m, a, b, c);
-			printf("원판이 3개일 때 총 횟수는 = %d\n",hanoiCount(m));
-			printf("m=4 : %d\nm=5 : %d\nm=6 : %d\nm=7 : %d\nm=8 : %d\nm=9 : %d\n",hanoiCount(m+1),hanoiCount(m+2),hanoiCount(m+3),
-			hanoiCount(m+4),hanoiCount(m+5),hanoiCount(m+6));
-			break;
-		default:
-			break;
-		}
-
+	printf("1: digitsum()\n2: binarySearch()\n3: hanoi()\n");
+	scanf("%d", &num);
+	switch (num)
+	{
+	case 1:
+		printf("정수를 입력하세요.\n");
+		scanf("%d", &n);
+		digitsum(n);
+		break;
+	case 2:
+		printf("찾을 값을 입력하세요.\n");
+		scanf("%d", &dec);
+		cnt = binarySearch(dec, first, last);
+		if (cnt >= 1)
+			printf("%d번째 탐색에서 찾음\n", cnt);
+		break;
+	case 3:
+		hanoi(m, a, b, c);
+		printf("원판이 3개일 때 총 횟수는 = %d\n", hanoiCount(m));
+		printf("m=4 : %d\nm=5 : %d\nm=6 : %d\nm=7 : %d\nm=8 : %d\nm=9 : %d\n", hanoiCount(m + 1), hanoiCount(m + 2), hanoiCount(m + 3),
+			   hanoiCount(m + 4), hanoiCount(m + 5), hanoiCount(m + 6));
+		break;
+	default:
+		break;
+	}
 
 	return 0;
-
-
-
 }
--- Data Struct/1주차_과제.c
+++ Data Struct/1주차_과제.c
@@ -1,11 +1,12 @@
-typedef struct point {
+typedef struct point
+{
 
 	int xpos;
 	int ypos;
-}Point;
+} Point;
 
-
-int Distance() {
+int Distance()
+{
 	int i = 0;
 
 	Point pos[10];
@@ -13,43 +14,48 @@ int Distance() {
 	int xdis = 0;
 	int ydis = 0;
 
-
 	printf("10개의 좌표를 입력하세요\n");
 
-	for (i = 0; i < 3; i++) {
+	for (i = 0; i < 3; i++)
+	{
 		scanf("%d %d", &pos[i].xpos, &pos[i].ypos);
 	}
 
-	for (i = 0; i < 3; i++) {
-		if (pos[i].xpos < pos[i + 1].xpos) {
+	for (i = 0; i < 3; i++)
+	{
+		if (pos[i].xpos < pos[i + 1].xpos)
+		{
 			if (xdis < pos[i + 1].xpos)
 				xdis = pos[i + 1].xpos;
 		}
-		else
-			if (xdis < pos[i].xpos) {
-				xdis = pos[i].xpos;
-			}
+		else if (xdis < pos[i].xpos)
+		{
+			xdis = pos[i].xpos;
+		}
 	}
-	for (i = 0; i < 3; i++) {
-		if (pos[i].ypos < pos[i + 1].ypos) {
+	for (i = 0; i < 3; i++)
+	{
+		if (pos[i].ypos < pos[i + 1].ypos)
+		{
 			if (ydis < pos[i + 1].ypos)
 				ydis = pos[i + 1].ypos;
 		}
-		else
-			if (ydis < pos[i].ypos) {
-				ydis = pos[i].ypos;
-			}
+		else if (ydis < pos[i].ypos)
+		{
+			ydis = pos[i].ypos;
+		}
 	}
-
 
 	int xsmall = pos[0].xpos;
 	int ysmall = pos[0].ypos;
 
-	for (i = 0; i < 3; i++) {
+	for (i = 0; i < 3; i++)
+	{
 		if (xsmall > pos[i].xpos)
 			xsmall = pos[i].xpos;
 	}
-	for (i = 0; i < 3; i++) {
+	for (i = 0; i < 3; i++)
+	{
 		if (ysmall > pos[i].ypos)
 			ysmall = pos[i].ypos;
 	}
@@ -61,15 +67,14 @@ int Distance() {
 	return 0;
 }
 
-int Prime() {
-
+int Prime()
+{
 
 	int inte;
 	int inte2;
-	int  i, j;
+	int i, j;
 	printf("정수 m과 n을 입력해주세요\n");
 	scanf("%d %d", &inte, &inte2);
-
 
 	for (i = inte; i <= inte2; i++)
 	{
@@ -80,52 +85,60 @@ int Prime() {
 			printf("%d ", i);
 	}
 	puts("\n\n");
-
 }
 
-int Birth() {
+int Birth()
+{
 
 	int i = 0;
 	int j = 0;
 	char birth1[9] = "20000101";
-	char late[9] = { 0 ,};
+	char late[9] = {
+		0,
+	};
 
 	late[9] = birth1;
 
-	printf("%s", late);/*
-	for (i = 0; i < 10; i++) {
+	printf("%s", late); /*
+	 for (i = 0; i < 10; i++) {
 
 
-		if (!strcmp(late, birth1[i])) {
-			continue;
-		}
-		else if (strcmp(late, birth1[i])) {
-			continue;
-		}
-		else
-			late[0] = birth1[i];
-	}
-	printf("%s\n", late[0]);*/
+		 if (!strcmp(late, birth1[i])) {
+			 continue;
+		 }
+		 else if (strcmp(late, birth1[i])) {
+			 continue;
+		 }
+		 else
+			 late[0] = birth1[i];
+	 }
+	 printf("%s\n", late[0]);*/
 	return 0;
 }
 
+int str_Birth()
+{
 
-int str_Birth() {
-
-	typedef struct birthday {
+	typedef struct birthday
+	{
 		int year[4];
 		int month[2];
 		int date[2];
 
-	}BIRTHDAY;
+	} BIRTHDAY;
 
-	BIRTHDAY birth[3] = { 0, };
+	BIRTHDAY birth[3] = {
+		0,
+	};
 	int i = 0;
-	int yearlate[] = { 0, };
+	int yearlate[] = {
+		0,
+	};
 	int monthlate = 0;
 	int datelate = 0;
 
-	for (i = 0; i < 2; i++) {
+	for (i = 0; i < 2; i++)
+	{
 		printf("4자리 태어난 연도 입력");
 		scanf("%d", &birth[i].year);
 		printf("4자리 태어난 달 입력");
@@ -134,61 +147,61 @@ int str_Birth() {
 		scanf("%d", &birth[i].date);
 	}
 
-	for (i = 0; i < 3; i++) {
+	for (i = 0; i < 3; i++)
+	{
 
-		if (yearlate <= birth[i].year) {
+		if (yearlate <= birth[i].year)
+		{
 			yearlate[0] = birth[i].year;
 			printf("%d  %d", yearlate[0], birth[i].year);
-			if (monthlate <= birth[i].month) {
+			if (monthlate <= birth[i].month)
+			{
 				monthlate = birth[i].month;
 
-				if (datelate <= birth[i].date) {
+				if (datelate <= birth[i].date)
+				{
 					datelate = birth[i].date;
 				}
 			}
-
 		}
 	}
 
 	printf("가장 늦은 생년월일은: %d\n\n", yearlate[0]);
-
 }
 
-
-int BirthArray() {
+int BirthArray()
+{
 
 	char birth[2][5];
 	int i, j = 0;
 	char late = 0;
 	printf("생년월일을 입력하세요");
 
-
-
-	for (i = 0; i < 2; i++) {
-		for (j = 0; j < 4; j++) {
+	for (i = 0; i < 2; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
 			scanf("%s", &birth[i][j]);
 		}
 	}
 
-	for (i = 0; i < 2; i++) {
-		for (j = 0; j < 4; j++) {
+	for (i = 0; i < 2; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
 			if ((strcmp(birth[i][j], birth[i][j + 1])) == 1)
 				late = birth[i][j];
 			else if ((strcmp(birth[i][j], birth[i][j + 1])) == -1)
 				late = birth[i][j + 1];
 			else
 				late = birth[i][j];
-
 		}
-
 	}
 
-
-
-
-
-	for (i = 0; i < 2; i++) {
-		for (j = 0; j < 4; j++) {
+	for (i = 0; i < 2; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
 			birth[i][j] = atoi(birth[i][j]);
 		}
 	}
@@ -196,14 +209,8 @@ int BirthArray() {
 	return 0;
 }
 
-
-
-
-
-
-
-
-int main() {
+int main()
+{
 	int num1;
 
 start:
@@ -223,7 +230,7 @@ start:
 		Birth();
 		goto start;
 	case 3:
-		//Birtharray();
+		// Birtharray();
 		goto start;
 	case 4:
 		str_Birth();
@@ -234,6 +241,4 @@ start:
 	default:
 		break;
 	}
-
-
 }
